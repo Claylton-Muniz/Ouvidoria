21feaa9dba064445004255a165965e9ad43343c2
Claylton-Muniz <claymuniz67@gmail.com>
2023-11-20 23:26:07 -0300

    Adicionando o layout do eficiência na página

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,5 +4,8 @@
 
 @section('content')
     <h1 class="h3 mb-2 text-gray-800">Dashboard</h1>
-    
+    <a href="ouvidoria">
+        <input class="btn btn-primary" type="button" value="Ouvidoria">
+    </a>
+
 @endsection
